a20429e9c52f1875104070374e98781cd4dea744
Jorge123456 <11522995+Jorge123456@users.noreply.github.com>
2020-03-08 15:01:31 +0000

    Add files via upload

--- Assets/StoreController.cs
+++ Assets/StoreController.cs
@@ -16,55 +16,59 @@ public class StoreController : MonoBehaviour
         
     }
 
-
     async public void PurchaseItem(){
+        
         string coinTokenID = "";
         string itemTokenID = "";
         string adminAddress = AccountManager.instance.adminAddress;
 
         EnjinItemParameter[] SendItem = new EnjinItemParameter[] { 
-        EnjinItemParameter.ConstructFungible(coinTokenID, 100);}
+        EnjinItemParameter.ConstructFungible(coinTokenID, 100)};
         EnjinItemParameter[] ReceiveItem = new EnjinItemParameter[] {
-        EnjinItemParameter.ConstructFungible(itemTokenID, 100);}
+        EnjinItemParameter.ConstructFungible(itemTokenID, 100)};
 
         IRequestHandler tradeCreationHandler =
         AccountManager.instance.userIdentity.CreateTradeItemRequestHandler(SendItem, ReceiveItem, adminAddress);
+        IRequestHandler tradeAcceptHandler = 
+        AccountManager.instance.adminIdentity.CompleteTradeItemRequestHandler(tradeID);
 
         await tradeCreationHandler.RegisterCallback(RequestEventType.CreateTradePending, (requestEvent) => {
         Debug.Log("Pending Done");
-        }
+        },
         await tradeCreationHandler.RegisterCallback(RequestEventType.CreateTradeBroadcast, (requestEvent) => {
         Debug.Log("Broadcasting Done");
-        }
+        },
         await tradeCreationHandler.RegisterCallback(RequestEventType.CreateTradeExecuted,
         async (requestEvent) => {
         string tradeID = requestEvent.Data.param1;
         Debug.Log("Create Trade Request Executed Done");
-        }
-
-        IRequestHandler tradeAcceptHandler = AccountManager.instance.adminIdentity
-        .CompleteTradeItemRequestHandler(tradeID);
+        },
 
         await tradeAcceptHandler.RegisterCallback(RequestEventType.CompleteTradePending,
         (completeRequestEvent) => {
             Debug.Log("Complete Trade Request Pending Done");
-        }
+        },
 
         await tradeAcceptHandler.RegisterCallback(RequestEventType.CompleteTradeBroadcast,
         (completeRequestEvent) => {
             Debug.Log("Complete Trade Request Broadcast Done");
-        }
+        },
         
-        await tradeAcceptHandler.RegisterCallback(RequestEventType.CompleteTradeExecuted,
-        (completeRequestEvent) => {
+        await tradeAcceptHandler.RegisterCallback(RequestEventType.CompleteTradeExecuted, 
+        async (completeRequestEvent) => {
             Debug.Log("Trade Done!!");
-        }
+        },
 
         await tradeAcceptHandler.Execute();
+        //  tradeAcceptHandler.Wait(1000),
         
-        
-        await  tradeCreationHandler.Execute();  
+        await tradeCreationHandler.Execute();
+        // tradeCreationHandler.Wait(1000);    
+    
+
     }
+        
+    
 }
         
 
